b3d12f0bebeadc7f2a11ce2cce29112247639421
64BitAsura <6957824+64BitAsura@users.noreply.github.com>
2023-07-02 03:18:57 +0000

    work in-progress

--- main.clj
+++ main.clj
@@ -2,6 +2,7 @@
   (:require
    [taoensso.tufte :as tufte :refer (defnp p profiled profile)]
    [tech.v3.tensor :as dtt]
+   [clojure.core.reducers :as r]
    [tech.v3.datatype :as dtype]
    [serializer.serial :as serializer]
    [seed.seed :as seed]
@@ -22,36 +23,58 @@
 (seed/load-seed "lang.vsa")
 
 (defn trigrams [text] (map (fn ([pos] (subs text pos (+ pos 3)))) (range 0 (- (count text) 2))))
+
 (defn extract-file [s]
   (second (re-find #"trainingData/(.+?)\.txt" s)))
+(defn zero [] 0)
+(defn lang-profiling [line] (reduce
+                             #(dtt/->tensor
+                               (vb/bundle-op
+                                (or (vb/get-hdv %1) %1) (vb/get-hdv %2))
+                               :container-type
+                               :jvm-heap
+                               :resource-type
+                               :gc
+                               :datatype
+                               :int)
+                             (trigrams (trim line))))
 
-(defn langProfiling [lang-hdv line] (reduce                                                                                                 (fn [acc gram]
-                                                                                                                                              (try                                                                                   (dtt/->tensor
-                                                                                                                                                                                                                                      (if (nil? acc)
-                                                                                                                                                                                                                                        (vb/get-hdv gram)
-                                                                                                                                                                                                                                        (vb/bundle (vb/get-hdv gram) acc))) (catch Exception _ (printf "failed on gram '%s  %s' %n" gram line))))
-                                                                                                                                            lang-hdv
-                                                                                                                                            (trigrams  (trim line))))
 (doseq [filePath (serializer/list-files "trainingData/")]
   (let [lang (str  "lang/" (extract-file filePath))]
     (when (nil? (vb/get-hdv lang))
       (tufte/profile {} (p :lang
                            (serializer/serialize-negative-vector-as-string
                             (lazy-seq [[lang
-                                        (vb/add-hdv! lang
-                                                     (reduce
-                                                      #(langProfiling %1 %2)
-                                                      nil
-                                                      (serializer/lazy-file-lines filePath)))]]) "lang.vsa"))))))
+                                        (dtt/->tensor
+                                         (vb/clip
+                                          (->>
+                                           (serializer/lazy-file-lines filePath)
+                                           (pmap lang-profiling)
+                                           (reduce vb/bundle-op)))
+                                         :container-type :native-heap
+                                         :resource-type nil
+                                         :datatype :byte)]]) "lang.vsa"))))))
 
-(def sample (langProfiling nil "a wiki is an online hypertext publication collaboratively edited and managed by its own audience using a web browser"))
+(seed/load-seed "lang.vsa")
+(def query (vb/bind (vb/bind (vb/protect-n (vb/get-hdv "t") 2) (vb/protect-n (vb/get-hdv "h") 1)) (vb/get-hdv "lang/eng")))
+(def lang-query (lang-profiling "den foerste valgmulighed at tage konventionernes indhold op til fornyet overvejelse for at fylde alle hullerne ud er en kompliceret og risikopraeget mulighed kompliceret fordi krigsfoerelsen til stadighed skifter karakter og risikopraeget fordi det ikke kan udelukkes at aabningen af konventionerne for nye forhandlinger kan foere til at der ikke opnaas nogen ny enighed"))
+(def lang-query2 (lang-profiling "eesistujariik alustab toeoed euroopa liidu jaoks kriitilisel ajal ja ma soovin talle edu samas tunnistan et mul on ka moningaid kartusi "))
+(def lang-query3 (lang-profiling "for many europeans a hens egg is a welcome part of their breakfast 
+"))
+
+(vb/reset-hdv-mem!)
+
+(seed/load-seed "seed.vsa")
+
+(println (vb/query-cleanup-mem-verbose query))
 
 (vb/reset-hdv-mem!)
 
 (seed/load-seed "lang.vsa")
 
-(println (vb/query-cleanup-mem sample))
-
+(println (vb/query-cleanup-mem lang-query))
+(println (vb/query-cleanup-mem lang-query2))
+(println (vb/query-cleanup-mem lang-query3))
 
 
 
--- src/serializer/tensor_str_tensor.test.clj
+++ src/serializer/tensor_str_tensor.test.clj
@@ -2,10 +2,13 @@
                                        [tech.v3.tensor :as dtt]
                                        [clojure.data.json :as json]
                                        [serializer.tensor-str-tensor]
-                                       [clojure.string]))
+                                       [clojure.string]
+                                       [seed.seed :as seed]
+                                       [seed.trigram :as trigram]
+                                       [gigasquid.vsa-base :as vb]))
 
-(def tensor (dtt/->tensor (repeatedly 10000 #(if (> (rand) 0.5) -1
-                                                 (if (> (rand) 0.5) 1 0))) :container-type :native-heap :resource-type nil :datatype :byte))
+(def tensor (dtt/->tensor (repeatedly 16 #(if (> (rand) 0.5) -1
+                                              (if (> (rand) 0.5) 1 0))) :container-type :native-heap :resource-type nil :datatype :byte))
 
 (def converted-str (json/write-str (serializer.tensor-str-tensor/convert-negative-vector-to-str tensor) :escape-unicode false :escape-js-separators false :escape-slash false))
 (def back-seq (serializer.tensor-str-tensor/convert-str-to-negative-vector (json/read-str converted-str)))
@@ -15,4 +18,4 @@
 (println "Back to sequence:" back-seq)
 (println "compare" (compare tensor back-seq))
 
-
+(println (vb/similarity-score (dtt/->tensor [-1 -1 0]) (dtt/->tensor [-1 -1 1])))
