b310832aef19f15c6ef1d1f02bae9251df480e89
64BitAsura <6957824+64BitAsura@users.noreply.github.com>
2023-09-20 14:56:10 +0000

    work in-progress

--- main.clj
+++ main.clj
@@ -88,7 +88,7 @@
 (def pos (atom {}))
 (def numeric (map int (into [] (concat  (range (int 0) (inc (int 110)))))))
 (run! #(vb/add-to-cleanup-mem % pos) numeric)
-(println (count pos))
+(println (count @pos))
 
 (doseq [filePath (serializer/list-files "testing/dicts")]
   (when (= filePath "testing/dicts/email-ids")
